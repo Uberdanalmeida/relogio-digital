84bb5a1cdbe3997f263fe3533cd3cc96b3cc5f14
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-07-03 23:47:53 -0300

    Deixando os numeros um do lado do outro ao invés de deixar um do lado do outro.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const relogio = setInterval(function teste() {
     const min = document.getElementById('min')
     const seg = document.getElementById('seg')
 
-    document.getElementById('hor').innerHTML = `${horas}`
-    document.getElementById('min').innerHTML = `${minutos}`
+    document.getElementById('hor').innerHTML = `${horas}:`
+    document.getElementById('min').innerHTML = `${minutos}:`
     document.getElementById('seg').innerHTML = `${segundos}`
 })
